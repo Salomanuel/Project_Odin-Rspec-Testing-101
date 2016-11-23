d57b5672b36e179027fa7220a2ac55394a584aaa
Manuel <salomanuel.aux+code@gmail.com>
2016-11-23 13:19:52 +0100

    start Test Driven Unicorns tutorial

---
+++ unicorns/unicorn_test.rb
@@ -0,0 +1,14 @@
+gem "minitest",	"~> 5.0"
+require "minitest/autorun"
+require "minitest/pride"
+
+class UnicornTest < Minitest::Test
+	def test_something
+	end
+
+	def test_something_else
+	end
+
+	def test_yet_another_thing
+	end
+end
