7e8394be6e5d2e31f18911ea02a05137e3a4223d
Manuel <salomanuel.aux+code@gmail.com>
2016-11-25 13:29:53 +0100

    new bunch of tests passed, such as accept new books, gets books by title and category

--- tutsplus2/lib/library.rb
+++ tutsplus2/lib/library.rb
@@ -3,4 +3,16 @@ class Library
 	def initialize(database=false)
 		@books = database ? YAML.load_file(database) : []
 	end
+
+	def get_books_in_category(category)
+		return @books.select{ |book| book.category == category }
+	end
+
+	def add_book(book)
+		@books << book
+	end
+
+	def get_book(title)
+		@books.each { |book| return book if book.title == title }
+	end
 end
--- tutsplus2/spec/library_spec.rb
+++ tutsplus2/spec/library_spec.rb
@@ -32,4 +32,14 @@ describe "Library object" do
 			end
 		end
 	end
+
+	it "returns all the books in a given category" do
+		expect(@lib.get_books_in_category(:development).length).to eql(2)
+	end
+
+	it "accepts new books" do
+		@lib.add_book( Book.new("Designing for the Web", "Mark Boulton", :design))
+		expect(@lib.get_book("Designing for the Web")).to be_an_instance_of(Book)
+	end
 end
+
