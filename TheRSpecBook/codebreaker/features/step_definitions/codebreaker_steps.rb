fc15955137603a56b3aca36a5510a34d99c13bce
Manuel <salomanuel.aux+code@gmail.com>
2016-12-28 13:12:25 +0100

    logical error reached, that means chapter 4 is done!

--- TheRSpecBook/codebreaker/features/step_definitions/codebreaker_steps.rb
+++ TheRSpecBook/codebreaker/features/step_definitions/codebreaker_steps.rb
@@ -1,6 +1,23 @@
+class DoubleOutput
+	def messages
+		@messages ||= []
+	end
+	def puts(message)
+		messages << message
+	end
+end
+def double_output
+	@output ||= DoubleOutput.new
+end
+
 Given /^I am not yet playing$/ do
 end
 
 When /^I start a new game$/ do
-	Codebreaker::Game.new.start
+	game = Codebreaker::Game.new(double_output)
+	game.start
+end
+
+Then /^I should see "([^"]*)"$/ do |message|
+	expect(double_output.messages).to include(message)
 end
