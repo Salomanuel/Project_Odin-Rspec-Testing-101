2b0608d03e4e21dae3bdb6dc0ec85c3e29d0777f
Manuel <salomanuel.aux+code@gmail.com>
2016-12-29 13:20:02 +0100

    I won't fix this: The gemspec tic_tac_toe.gemspec is not valid. Please fix this gemspec

--- tic_tac_toe/spec/cell_spec.rb
+++ tic_tac_toe/spec/cell_spec.rb
@@ -2,5 +2,12 @@ require "spec_helper"
 
 module TicTacToe
 	describe Cell do
+		context "#initialize" do
+			it "is initialized with a value of '' by default" do
+				cell = Cell.new
+				expect(cell.value).to eq ''
+			end
+		end
+
 	end
 end
